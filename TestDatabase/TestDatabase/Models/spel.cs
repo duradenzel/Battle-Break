76ec037a2431c0fa30ed53f9811f9816add9f3e4
Ouden,Silvan S. den <silvan.denouden@student.fontys.nl>
2023-04-04 15:43:44 +0200

    pure aids

---
+++ TestDatabase/TestDatabase/Models/spel.cs
@@ -0,0 +1,23 @@
+﻿namespace TestDatabase.Models
+{
+    public class spel
+    {
+        public string naam;
+        public int minimumSpelers;
+        public string regels;
+        public string winConiditie;
+
+        public spel()
+        {
+
+        }
+
+        public spel(string naam, int minimumSpelers, string regels, string winConiditie)
+        {
+            this.naam = naam;
+            this.minimumSpelers = minimumSpelers;
+            this.regels = regels;
+            this.winConiditie = winConiditie;
+        }
+    }
+}
