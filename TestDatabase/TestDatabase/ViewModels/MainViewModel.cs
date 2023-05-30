903e08fbbab3c19765b55f926c161e7080e8ef27
Ouden,Silvan S. den <silvan.denouden@student.fontys.nl>
2023-05-30 14:17:49 +0200

    yeayhhhhh

--- TestDatabase/TestDatabase/ViewModels/MainViewModel.cs
+++ TestDatabase/TestDatabase/ViewModels/MainViewModel.cs
@@ -6,11 +6,13 @@ namespace TestDatabase.ViewModels
     {
         public List<LeaderboardModel> Leaderboard { get; set; }
         public List<MatchHistoryModel> MatchHistory { get; set; }
+        public List<GameModel> GameList { get; set; }
 
-        public MainViewModel(List<LeaderboardModel> leaderboardModels, List<MatchHistoryModel> matchHistoryModels)
+        public MainViewModel(List<LeaderboardModel> leaderboardModels, List<MatchHistoryModel> matchHistoryModels, List<GameModel> gameList)
         {
             Leaderboard = leaderboardModels;
             MatchHistory = matchHistoryModels;
+            GameList = gameList;
         }
     }
 }
